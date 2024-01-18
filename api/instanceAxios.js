a681d0c565d5e22d24fc7b56dae28718cca64b15
Valerie K <valerik210695@gmail.com>
2024-01-18 15:39:16 +0300

    deploy

--- api/instanceAxios.js
+++ api/instanceAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
   // baseURL: "http://localhost:3001",
-  baseURL: "http://158.160.142.248",
+  baseURL: "http://158.160.142.248/api",
   proxy: true,
   withCredentials: true,
   headers: { "Access-Control-Allow-Origin": "*" },
